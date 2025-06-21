3dbb8283f3b7ecc160fe16cba123118f9a9524ff
Krapaud <mur.mickael@gmail.com>
2025-06-21 12:23:19 +0200

    Add 4-puts.c to print a programming quote

---
+++ 4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Print exactly
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
