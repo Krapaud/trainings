e6638ac6634dafc81b0ed6a37df8435e303a0b9b
Krapaud <mur.mickael@gmail.com>
2025-06-29 00:38:01 +0200

    Add puts2 and puts_half functions to print characters at even indices and half of a string

---
+++ trainings_4/6-puts2.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+void puts2(char *str)
+{
+	int len = 0;
+
+	while (str[len] != '\0')
+	{
+		if (len % 2 == 0)
+			_putchar(str[len]);
+		len++;
+	}
+	_putchar('\n');
+}
